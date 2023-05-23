bc27deb11cebdec1248d8e1fd535948dc7d0a785
himaloyash <himaloyashis98@gmail.com>
2023-05-23 23:22:43 +0600

    bistro boss done

--- src/home/Bistro/BistroBoss.jsx
+++ src/home/Bistro/BistroBoss.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './BistroBoss.css'
 
+
 const BistroBoss = () => {
     return (
-        <div className='bistro'>
-            
+        <div className='bistro bg-slate-50 bg-fixed mx-auto text-black p-10'>
+            <div className=' w-2/3 mx-auto'>
+                <h1>Bistro Boss</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus amet omnis et voluptate provident ut consectetur, a, id repellendus quasi quo sit ducimus harum unde eveniet consequuntur tempora. Totam odio voluptate repellat voluptatem iure fuga maxime beatae rem eos culpa? Dolore cumque consequuntur id expedita quo at nobis sunt facilis!</p>
+            </div>
+
         </div>
     );
 };
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,12 +4,14 @@ import Category from './Category/Category';
 import PopularItem from '../Pages/PopularItem/PopularItem';
 import Featured from '../Pages/Featured/Featured';
 import Testimonial from './Testimonial/Testimonial';
+import BistroBoss from './Bistro/BistroBoss';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Category></Category>
+            <BistroBoss></BistroBoss>
             <PopularItem></PopularItem>
             <Featured></Featured>
             <Testimonial></Testimonial>
