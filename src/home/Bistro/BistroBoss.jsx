cdb8aa4543682fa36046d7a6822df007902abd35
himaloyash <himaloyashis98@gmail.com>
2023-05-23 22:49:01 +0600

    react carosuel added

---
+++ src/home/Bistro/BistroBoss.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BistroBoss.css'
+
+const BistroBoss = () => {
+    return (
+        <div className='bistro'>
+            
+        </div>
+    );
+};
+
+export default BistroBoss;
--- src/home/Testimonial/Testimonial.jsx
+++ src/home/Testimonial/Testimonial.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../components/SectiionTitle/SectionTitle';
 import { Swiper, SwiperSlide } from "swiper/react";
+import '@smastrom/react-rating/style.css'
 
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
+import { Rating } from '@smastrom/react-rating';
 
 const Testimonial = () => {
 
     const [reviews, setReviews] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('reviews.json')
-        .then(res=>res.json())
-        .then(data=>setReviews(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
 
     return (
         <div>
@@ -26,17 +28,21 @@ const Testimonial = () => {
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
 
                 {
-                    reviews.map(review=>(<SwiperSlide key={review._id}>
+                    reviews.map(review => (<SwiperSlide key={review._id}>
 
-                        <div className='p-20'>
-                            
+                        <div className='mx-20 py-10 space-y-2 flex flex-col items-center'>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
                             <p>{review.details}</p>
                             <p className='text-yellow-500'>{review.name}</p>
                         </div>
                     </SwiperSlide>))
                 }
-                
-                
+
+
             </Swiper>
 
         </div>
