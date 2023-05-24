a2c01337ad32d6e0c137b144d17db77bf114ed3d
himaloyash <himaloyashis98@gmail.com>
2023-05-24 09:14:19 +0600

    react async added

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,10 +6,16 @@ import Featured from '../Pages/Featured/Featured';
 import Testimonial from './Testimonial/Testimonial';
 import BistroBoss from './Bistro/BistroBoss';
 import ContactUs from './Contact/ContactUs';
+import { Helmet } from 'react-helmet-async';
+
 
 const Home = () => {
     return (
         <>
+            <Helmet>
+                <title>Home</title>
+                
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <BistroBoss></BistroBoss>
--- src/home/Menu/Menu.jsx
+++ src/home/Menu/Menu.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Menu = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Home</title>
+                
+            </Helmet>
         </div>
     );
 };
