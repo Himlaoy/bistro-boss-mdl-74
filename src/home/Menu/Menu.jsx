ddad0500dbc280bc9fab2f5c9e2baf88f0d828d8
himaloyash <himaloyashis98@gmail.com>
2023-05-24 10:30:37 +0600

    popular item

--- src/home/Menu/Menu.jsx
+++ src/home/Menu/Menu.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Pages/Cover/Cover';
+import img1 from '../../assets/menu/banner3.jpg'
+import PopularItem from '../../Pages/PopularItem/PopularItem';
 
 const Menu = () => {
     return (
         <div>
             <Helmet>
-                <title>Home</title>
+                <title>Bistro | Home</title>
                 
             </Helmet>
+            <Cover title={'Our menu'} img={img1}></Cover>
+            <PopularItem></PopularItem>
+            <PopularItem></PopularItem>
+            <PopularItem></PopularItem>
         </div>
     );
 };
