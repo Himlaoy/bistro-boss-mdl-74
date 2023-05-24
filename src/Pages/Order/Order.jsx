10290cb59ccaa96b4871b9bad1e8536bd5ab1d36
himaloyash <himaloyashis98@gmail.com>
2023-05-24 14:03:08 +0600

    orderCard added

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -3,19 +3,33 @@ import Cover from '../Cover/Cover';
 import orderImg from '../../assets/shop/banner2.jpg'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../Hooks/useMenu';
 
 const Order = () => {
 
     const [taIndex, setTabIndex] = useState(0)
+    const [menu] = useMenu([])
+
+    const Dessert = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
+
 
     return (
         <div>
             <Cover img={orderImg} title={'Order Food'}></Cover>
             <Tabs defaultIndex={taIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Drink</Tab>
                 </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
             </Tabs>
