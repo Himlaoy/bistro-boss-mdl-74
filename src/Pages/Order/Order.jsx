3303a542b8427352f0f2aa37690c6d333ebd8924
himaloyash <himaloyashis98@gmail.com>
2023-05-24 13:53:23 +0600

    react tab added

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cover from '../Cover/Cover';
 import orderImg from '../../assets/shop/banner2.jpg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
+
+    const [taIndex, setTabIndex] = useState(0)
+
     return (
         <div>
-            <Cover img={orderImg } title={'Order Food'}></Cover>
+            <Cover img={orderImg} title={'Order Food'}></Cover>
+            <Tabs defaultIndex={taIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Title 1</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
