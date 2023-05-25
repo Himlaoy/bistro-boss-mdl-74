61a876fb6a5920897f795faae9ad00c46028e1b3
himaloyash <himaloyashis98@gmail.com>
2023-05-25 10:37:23 +0600

    create user and signin

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,20 +1,44 @@
-import React, { createContext, useState } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
-const [user, setUser] = useState()
-const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn =(email, password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email, password)
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('current user', currentUser)
+            setUser(currentUser)
+            setLoading(false)
+
+        })
+        return ()=>{
+            return unSubscribe()
+        }
+    },[])
 
 
 
-    const authInfo= {
+    const authInfo = {
         user,
         loading,
+        createUser,
+        signIn,
     }
 
 
