a6312506248c21ee7d59cde159a00b37d9094bbe
himaloyash <himaloyashis98@gmail.com>
2023-05-28 17:15:13 +0600

    I'm undone

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
 
 const useCart = () => {
+    const {user} = useContext(AuthContext)
+    const { refetch, data: cart = []} = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async()=>{
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return res.json()
+        }
+    })
+
+    return [cart, refetch]
+
+
     
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,18 @@ import router from './Routes/Routes.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provider/AuthProvider.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <QueryClientProvider client={QueryClient}>
+        <QueryClientProvider client={queryClient}>
           <div className='max-w-screen-lg mx-auto '>
             <RouterProvider router={router}></RouterProvider>
           </div>
         </QueryClientProvider>
+
 
       </HelmetProvider>
     </AuthProvider>
